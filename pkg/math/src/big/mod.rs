dce4d2aafd3a8cb6043e134f4c46123cca8ae8b3
Dennis Shtatnov <densht@gmail.com>
2022-09-26 17:53:38 -0700

    Move modulo to separate file.

--- pkg/math/src/big/mod.rs
+++ pkg/math/src/big/mod.rs
@@ -1,7 +1,9 @@
 mod int;
+mod modulo;
 mod secure;
 mod uint;
 
 pub use self::int::*;
+pub use self::modulo::*;
 pub use self::secure::*;
 pub use self::uint::*;
---
+++ pkg/math/src/big/modulo.rs
@@ -0,0 +1,116 @@
+use crate::big::uint::BigUint;
+
+/// A set of operations which all result in a 'mod n' result.
+/// TODO: This would ideally implement operations which have intermediate
+/// results bounded by the size of the modulus.
+pub struct Modulo<'a> {
+    pub n: &'a BigUint,
+}
+
+// sub_assign(self, rhs: &BigUint)
+// sub_to(&self, rhs: &BigUint, out: &bigUint)
+
+impl<'a> Modulo<'a> {
+    pub fn new(n: &'a BigUint) -> Self {
+        Modulo { n }
+    }
+
+    pub fn rem(&self, a: &BigUint) -> BigUint {
+        a % self.n
+    }
+
+    pub fn add(&self, a: &BigUint, b: &BigUint) -> BigUint {
+        (a + b) % self.n
+    }
+
+    pub fn add_into(&self, mut a: BigUint, b: &BigUint) -> BigUint {
+        a += b;
+        a % self.n
+    }
+
+    pub fn sub(&self, a: &BigUint, b: &BigUint) -> BigUint {
+        (((a % self.n) + self.n) - (b % self.n)) % self.n
+    }
+
+    // TODO: Even more efficient is b is also owned
+    pub fn sub_into(&self, mut a: BigUint, b: &BigUint) -> BigUint {
+        a = a % self.n;
+        a += self.n;
+        a -= b % self.n;
+        a = a % self.n;
+        a
+    }
+
+    pub fn mul(&self, a: &BigUint, b: &BigUint) -> BigUint {
+        (a * b) % self.n
+    }
+
+    /// Computes a^b mod n
+    pub fn pow(&self, a: &BigUint, b: &BigUint) -> BigUint {
+        if b.is_zero() {
+            return BigUint::from(1);
+        }
+        if b.is_one() {
+            return a.clone();
+        }
+
+        let mut out = BigUint::from(1);
+        let mut p = a.clone();
+        for i in 0..b.nbits() {
+            if b.bit(i) == 1 {
+                out = self.mul(&out, &p);
+            }
+            p = self.mul(&p, &p);
+        }
+
+        out
+    }
+
+    /// Computes the modular inverse 'a^-1' such the 'a*(a^-1) = 1 mod n'.
+    ///
+    /// Algorithm is equivalent to the following (but using modular arithmetic
+    /// instead of signed arithmetic): https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Modular_integers
+    pub fn inv(&self, a: &BigUint) -> BigUint {
+        let mut t = BigUint::zero();
+        let mut new_t = BigUint::from(1);
+        let mut r = self.n.clone();
+        let mut new_r = a.clone();
+
+        while !new_r.is_zero() {
+            let (q, rem) = r.quorem(&new_r);
+            tup!((t, new_t) = (new_t.clone(), self.sub(&t, &(&q * &new_t))));
+            tup!((r, new_r) = (new_r.clone(), rem));
+        }
+
+        if r > BigUint::from(1) {
+            panic!("Not invertible");
+        }
+
+        t
+    }
+
+    /// Computes '(a / b) mod n'.
+    /// Internally performs '(a * b^-1) mod n'
+    pub fn div(&self, a: &BigUint, b: &BigUint) -> BigUint {
+        self.mul(a, &self.inv(b))
+    }
+
+    /// Computes '-1*a mod n'
+    pub fn negate(&self, a: &BigUint) -> BigUint {
+        self.sub(self.n, a)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn modulo_test() {
+        let p = BigUint::from(7);
+        let m = Modulo::new(&p);
+        let x = m.inv(&2.into());
+        assert_eq!(x, BigUint::from(4));
+        assert_eq!(m.div(&1.into(), &2.into()), BigUint::from(4));
+    }
+}
